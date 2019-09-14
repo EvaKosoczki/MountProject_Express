40774d50a8a554d47b144db060a93744f534c034
Kosoczki Eva <eva.kosoczki@gmail.com>
2019-09-14 21:32:07 +0200

    all Mountains to Angular

--- routes/api.js
+++ routes/api.js
@@ -3,20 +3,31 @@ var router = express.Router();
 let MountDB = require('../db/mountDb')
 let MountDBSmp = new MountDB();
 
+
+router.get('/mount', async (req, res, next) => {
+  let allMounts = await MountDBSmp.getMountains();
+  res.json(allMounts);
+})
+
 router.get('/:id', async (req, res, next) => {
   let data = await MountDBSmp.readUserProfil(req.params.id);
   res.json(data);
 
 })
 
+
+
 router.post('/new', async (req, res, next) => {
   let newuser = await MountDBSmp.createUser(req.body);
   res.json(newuser);
 })
+
 router.post('/:id', async (req, res, next) => {
   let editeduser = await MountDBSmp.updateUserProfil(req.body);
   res.json(editeduser);
 })
+
+
 
 
 module.exports = router;
